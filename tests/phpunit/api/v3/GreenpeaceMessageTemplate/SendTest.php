326b859e99070a7953d673c9cfa8da4e2b0c902f
Viliam Ivanov <viliam.ivanov@greenpeace.org>
2022-07-12 10:32:35 +0200

    GP-30260 GreenpeaceMessageTemplate.send API

---
+++ tests/phpunit/api/v3/GreenpeaceMessageTemplate/SendTest.php
@@ -0,0 +1,82 @@
+<?php
+
+use Civi\Test\HeadlessInterface;
+use Civi\Test\HookInterface;
+use Civi\Test\TransactionalInterface;
+
+/**
+ * GreenpeaceMessageTemplate.Send API Test Case
+ * This is a generic test class implemented with PHPUnit.
+ * @group headless
+ */
+class api_v3_GreenpeaceMessageTemplate_SendTest extends \PHPUnit\Framework\TestCase implements HeadlessInterface, HookInterface, TransactionalInterface {
+  use \Civi\Test\Api3TestTrait;
+
+  /**
+   * Set up for headless tests.
+   *
+   * Civi\Test has many helpers, like install(), uninstall(), sql(), and sqlFile().
+   *
+   * See: https://docs.civicrm.org/dev/en/latest/testing/phpunit/#civitest
+   */
+  public function setUpHeadless() {
+    return \Civi\Test::headless()
+      ->installMe(__DIR__)
+      ->apply();
+  }
+
+  /**
+   * The setup() method is executed before the test is executed (optional).
+   */
+  public function setUp() {
+    parent::setUp();
+  }
+
+  /**
+   * The tearDown() method is executed after the test was executed (optional)
+   * This can be used for cleanup.
+   */
+  public function tearDown() {
+    parent::tearDown();
+  }
+
+  /**
+   * Test invalid email.
+   */
+  public function testInvalidEmail() {
+    $result = civicrm_api3('GreenpeaceMessageTemplate', 'send', [
+      'id' => 1,
+      'to_emails' => 'abc123'
+    ]);
+    $this->assertEquals('abc123', $result['values']['invalid'][0]);
+  }
+
+  /**
+   * Test valid email.
+   */
+  public function testValidEmail() {
+    $result = civicrm_api3('GreenpeaceMessageTemplate', 'send', [
+      'id' => 1,
+      'to_emails' => 'abc123@test.com'
+    ]);
+    $this->assertEquals('abc123@test.com', $result['values']['valid'][0]);
+  }
+
+  /**
+   * Test valid emails (comma separated list).
+   */
+  public function testValidEmails() {
+    $emails = [
+      'abc123@test.com',
+      'xyz123@test.com',
+      'test@gmail.com'
+    ];
+    $result = civicrm_api3('GreenpeaceMessageTemplate', 'send', [
+      'id' => 1,
+      'to_emails' => implode(',', $emails)
+    ]);
+    $this->assertEquals($emails[0], $result['values']['valid'][0]);
+    $this->assertEquals($emails[1], $result['values']['valid'][1]);
+    $this->assertEquals($emails[2], $result['values']['valid'][2]);
+  }
+}
