cab66f5d94c12cadebfa9268d28e47caae45385a
Patrick Figel <pfigel@greenpeace.org>
2024-01-09 18:08:44 +0100

    GP-44377 Change civioffice permissions

--- Civi/Api4/UimodsTemplate.php
+++ Civi/Api4/UimodsTemplate.php
@@ -10,4 +10,9 @@ namespace Civi\Api4;
  */
 class UimodsTemplate extends Generic\DAOEntity {
 
+  public static function permissions() {
+    return [
+      'default'    => ['view all contacts'],
+    ];
+  }
 }
